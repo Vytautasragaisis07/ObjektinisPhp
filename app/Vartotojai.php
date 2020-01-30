f183366b3fa19a5442610d8353e5a3f0bbf70bc5
Titas <titasdg@gmail.com>
2020-01-30 15:30:17 +0200

    first commit

--- app/Vartotojai.php
+++ app/Vartotojai.php
@@ -3,38 +3,32 @@
  * Created by PhpStorm.
  * User: moksleivis
  * Date: 2020-01-30
- * Time: 13:40
+ * Time: 15:15
  */
 
 namespace OOP;
 
 
-abstract class Bendruomene
+abstract class Vartotojai
 {
     protected $name;
     protected $email;
     protected $phone;
 
-     public function __construct($name, $email, $phone){
+    abstract public function __construct($name, $email, $phone){
         $this->name = $name;
         $this->email = $email;
         $this->phone = $phone;
 
     }
-    public function profile(){
-        $data [] = $this->name;
-        $data [] = $this->email;
-        $data [] = $this->phone;
-
-        return $data;
-    }
-    function addComment($comment){
+    abstract function addComment($comment){
         $this->comment = $comment;
     }
-    public function showComment()
+    abstract public function showComment()
     {
         $commentInfo = $this->comment;
 
         return $commentInfo;
     }
+}
 }
